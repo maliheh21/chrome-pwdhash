7731dc52de0d74f2e757f40735a535e4eb77dc13
c.lioukeeon <c.lioukeeon@localhost>
2010-02-06 14:25:36 +0000

    almost ready to release

---
+++ null.js
@@ -0,0 +1,14 @@
+
+var NullFunction = function(){};
+var NullConsole = {dir: function(){}, log: function(){}};
+
+var NullKeyHooker = function (field) {
+	this.intercept = NullFunction;
+	this.stop = NullFunction;
+	this.setValue = function (str) {
+		return str;
+	};
+	this.getValue = function () {
+		return field.value;
+	};
+};
